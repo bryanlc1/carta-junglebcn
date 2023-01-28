b61478996a0fd271ad9f13c1d8ad4fa833561600
Bryan Lojan <bryanlc9808@gmail.com>
2023-01-28 22:31:20 +0100

    create menu

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="container">
+      <img src="public/prueba1.png" className='image' alt="bebidas-cervezas-cubos" />
+      <img src="public/prueba2.png" className='image' alt="cocktails" />
+      <img src="public/prueba3.png" className='image' alt="combinados" />
+      <img src="public/prueba4.png" className='image' alt="tapas" />
+    </div>
+    </>
+  )
+}
+
+export default App
